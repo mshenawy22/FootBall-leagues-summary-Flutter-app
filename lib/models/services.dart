47c47be30a07b33d4511e4e31f9a1a913d161fce
jay shenawy <jay.shenawy@netxtra.net>
2022-01-10 13:42:57 +0000

    cleaning up services.dart

--- lib/models/services.dart
+++ lib/models/services.dart
@@ -4,35 +4,22 @@ import 'package:equatable/equatable.dart';
 import 'package:http/http.dart' as http;
 
 import 'matches_history.dart';
-
- class CountaWin{
-   NumberofWins(){
-   count =  (count ==0)? 0 : count++;
-   }
-  static int count =0;
-}
+import 'dart:developer' as developer;
 
 
 class Team extends Equatable{
-  Team({this.id, this.name});
+  Team({required  this.name , required  this.numofWins});
 
-  final int? id ;
-  final String? name;
+  final String name;
+  final int numofWins;
 
   @override
-  List<Object?> get props =>[this.id , this.name];
+  List<Object?> get props =>[ this.name , this.numofWins];
 }
 
 
 class  MatchServices   {
-
-    MatchesHistory? matchesHistory;
-    Map matchWinners = Map <String, int>();
-
- 
- 
- 
-
+  late Team _teamWithMostWins ;
 
   static const _baseUrl = 'api.football-data.org';
   static const String _GET_MATCHES = '/v2/competitions/2021/matches/';
@@ -43,9 +30,12 @@ class  MatchServices   {
     'status'   :  'FINISHED'
   };
 
+
+
   Future<MatchesHistory> getMatchesList() async {
     Uri uri = Uri.https(_baseUrl, _GET_MATCHES , _QUERY_PARAMS);
-    print (uri);
+
+    developer.log(uri.toString(), name: 'just logging');
 
     final response = await http.get(
       uri,
@@ -53,35 +43,49 @@ class  MatchServices   {
         'X-Auth-Token': 'cd029676c88b495da48839aefb25702c',
       },
     );
-    matchesHistory = await MatchesHistory.fromJson(response.body);
-    return matchesHistory! ;
+
+    MatchesHistory _matchesHistory = await MatchesHistory.fromJson(response.body);
+    return _matchesHistory ;
+
+  }
+
+  computeTeamWithMostWins() async
+  {
+    var _matchesHistory = await getMatchesList();
+    var matchWinners = await createWinnerTable(_matchesHistory);
+    matchWinners     = sortWinnerTablesbyWins(matchWinners);
+    _teamWithMostWins = Team (name : matchWinners.entries.first.key, numofWins: matchWinners.entries.first.value) ;
+
+
+    print('Team with most wins is ${_teamWithMostWins.name}  with ${_teamWithMostWins.numofWins} wins  ');
 
   }
 
 
-  createWinnerTable()
+  createWinnerTable(MatchesHistory _matchesHistory ) async
   {
+    Map matchWinners = Map <String, int>();
 
-    
-    if (matchesHistory != null)
+    if (_matchesHistory != null)
     {
 
-      matchesHistory!.matches?.forEach((match) {
+
+      _matchesHistory.matches!.forEach((match) {
 
         switch (match.score!.winner) {
-          
+
           case Winner.AWAY_TEAM:
             // matchWinners[Team(id : match.awayTeam?.id ,name: match.awayTeam?.name)]  ==null ? 0 :  CountaWin() ;
             matchWinners[match.awayTeam?.name] =
             ( matchWinners[match.awayTeam?.name] == null ?0 : ++matchWinners[match.awayTeam?.name]);
-            // print ( '${match.awayTeam!.name} : No of wons  ${  matchWinners[match.awayTeam?.name]}');
+            // developer.log( ( '${match.awayTeam!.name} : No of wons  ${  matchWinners[match.awayTeam?.name]}');
             break;
 
           case Winner.HOME_TEAM:
             matchWinners[match.homeTeam?.name] =
             ( matchWinners[match.homeTeam?.name] == null ? 0 : ++matchWinners[match.homeTeam?.name]);
 
-             // print('${match.homeTeam?.name} : No of wons ${matchWinners[match.homeTeam?.name]}');
+             // developer.log('${match.homeTeam?.name} : No of wons ${matchWinners[match.homeTeam?.name]}');
             break;
           case Winner.DRAW:
             //do nothing
@@ -90,18 +94,34 @@ class  MatchServices   {
 
       });
     }
+    else {
+      developer.log('null _matchesHistory', name: 'null Errors' );
+
+    }
+
+    return matchWinners;
 
 
   }
 
-  sortWinnerTablesbyWins()
+  sortWinnerTablesbyWins(Map <String, int> matchWinners)
   {
-    
+
+      var sortedEntries = matchWinners.entries.toList()..sort((e1, e2) {
+        var diff = e2.value.compareTo(e1.value);
+        if (diff == 0) diff = e2.key.compareTo(e1.key);
+        return diff;
+      });
+
+      matchWinners..clear()..addEntries(sortedEntries);
+
+      return matchWinners;
   }
 
-
-
-
+  Team teamWithMostWins()
+  {
+    return _teamWithMostWins ;
+  }
 
 
 
