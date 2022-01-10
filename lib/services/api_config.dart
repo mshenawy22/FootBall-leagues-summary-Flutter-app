3ac2f00c78bc9097cf3b225c8adc149ddc33943f
jay shenawy <jay.shenawy@netxtra.net>
2022-01-10 21:18:23 +0000

    a config file for api strings

---
+++ lib/services/api_config.dart
@@ -0,0 +1,4 @@
+const String BASE_URL = 'api.football-data.org';
+const String  COMPETITION_ID = '2002';
+const String  GET_MATCHES = '/v2/competitions/${COMPETITION_ID}/matches/';
+const String  GET_TEAM = '/v2/teams/';
