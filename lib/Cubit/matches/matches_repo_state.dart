84a05de9ab69c5ac3cb85af6ca060fe55e758abd
jay shenawy <jay.shenawy@netxtra.net>
2022-01-10 23:51:58 +0000

    cleanup

--- lib/Cubit/matches/matches_repo_state.dart
+++ lib/Cubit/matches/matches_repo_state.dart
@@ -1,7 +1,7 @@
 part of 'matches_repo_cubit.dart';
 
 
-enum MatchesRepoStatus { INITIAL , FETCHING_MATCHES, WINNER_COMPUTED, FETCHING_TEAM_INFO , TEAM_INFO_SUCCESS, FAILURE }
+enum MatchesRepoStatus { INITIAL , FETCHING_MATCHES, WINNER_COMPUTED, FAILURE }
 
 class MatchesRepoState {
   Team? teamWithMostWins;
