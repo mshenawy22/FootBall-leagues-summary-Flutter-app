15f1e3b567127a0d8b418a07fe0cefd5aa14bc50
jay shenawy <jay.shenawy@netxtra.net>
2022-01-11 15:58:42 +0000

    api configs

--- lib/services/api_config.dart
+++ lib/services/api_config.dart
@@ -1,4 +1,7 @@
 const String BASE_URL = 'api.football-data.org';
-const String  COMPETITION_ID = '2002';
+const String PRIMERLEAGUE = '2021';
+const String SERIEA = '2019';
+const String BUNDESLIGA = '2020';
+const String  COMPETITION_ID = SERIEA;
 const String  GET_MATCHES = '/v2/competitions/${COMPETITION_ID}/matches/';
 const String  GET_TEAM = '/v2/teams/';
